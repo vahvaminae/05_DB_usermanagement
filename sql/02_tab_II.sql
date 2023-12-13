7446030559bec810c4594e31ad4e106170e13080
vahvaminae <JenifferLeutert@gmx.de>
2023-12-13 11:02:20 +0000

    Groß - & Kleinschreibung angepasst

--- sql/02_tab_II.sql
+++ sql/02_tab_II.sql
@@ -1,24 +1,24 @@
-DROP TABLE IF EXISTS mydb.Countries;
+DROP TABLE IF EXISTS mydb.countries;
 
-CREATE TABLE mydb.Countries (
+CREATE TABLE mydb.countries (
   id INT NOT NULL AUTO_INCREMENT,
-  Country VARCHAR(100) NOT NULL,
-  Capital VARCHAR(100) NOT NULL,
-  Population INT NOT NULL,
+  country VARCHAR(100) NOT NULL,
+  capital VARCHAR(100) NOT NULL,
+  population INT NOT NULL,
   PRIMARY KEY (id)
 );
 
-DESCRIBE mydb.Countries;
+DESCRIBE mydb.countries;
 
-INSERT INTO mydb.Countries (Country, Capital, Population) VALUES ('Egypt', 'Cairo', 100000000);
-INSERT INTO mydb.Countries (Country, Capital, Population) VALUES ('Saudi Arabia', 'Riyadh', 34000000);
-INSERT INTO mydb.Countries (Country, Capital, Population) VALUES ('Iraq', 'Baghdad', 40000000);
-INSERT INTO mydb.Countries (Country, Capital, Population) VALUES ('Morocco', 'Rabat', 36000000);
-INSERT INTO mydb.Countries (Country, Capital, Population) VALUES ('Algeria', 'Algiers', 43000000);
-INSERT INTO mydb.Countries (Country, Capital, Population) VALUES ('Tunisia', 'Tunis', 12000000);
-INSERT INTO mydb.Countries (Country, Capital, Population) VALUES ('Jordan', 'Amman', 10000000);
-INSERT INTO mydb.Countries (Country, Capital, Population) VALUES ('Lebanon', 'Beirut', 7000000);
-INSERT INTO mydb.Countries (Country, Capital, Population) VALUES ('Oman', 'Muscat', 5000000);
-INSERT INTO mydb.Countries (Country, Capital, Population) VALUES ('Qatar', 'Doha', 3000000);
+INSERT INTO mydb.countries (country, capital, population) VALUES ('Egypt', 'Cairo', 100000000);
+INSERT INTO mydb.countries (country, capital, population) VALUES ('Saudi Arabia', 'Riyadh', 34000000);
+INSERT INTO mydb.countries (country, capital, population) VALUES ('Iraq', 'Baghdad', 40000000);
+INSERT INTO mydb.countries (country, capital, population) VALUES ('Morocco', 'Rabat', 36000000);
+INSERT INTO mydb.countries (country, capital, population) VALUES ('Algeria', 'Algiers', 43000000);
+INSERT INTO mydb.countries (country, capital, population) VALUES ('Tunisia', 'Tunis', 12000000);
+INSERT INTO mydb.countries (country, capital, population) VALUES ('Jordan', 'Amman', 10000000);
+INSERT INTO mydb.countries (country, capital, population) VALUES ('Lebanon', 'Beirut', 7000000);
+INSERT INTO mydb.countries (country, capital, population) VALUES ('Oman', 'Muscat', 5000000);
+INSERT INTO mydb.countries (Country, Capital, Population) VALUES ('Qatar', 'Doha', 3000000);
 
-SELECT * FROM mydb.Countries;
+SELECT * FROM mydb.countries;
